b1da9ca7c020979a6b585e174b50f4a5f3da0693
Wilbert Chan <l22070034@valladolid.tecnm.mx>
2026-04-21 17:49:29 -0600

    configuracion .env
    implementamos la funcionalidad de nuestro conexion con supebase

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,19 @@
-module.exports = {
-    presets: ['babel-preset-expo'],
-    plugins: [
-        ['module:react-native-dotenv', {
-            moduleName: '@env',
-            path: '.env.local',
-        }]
-    ]
+module.exports = function (api) {
+    api.cache(true);
+    return {
+        presets: ['babel-preset-expo'],
+        plugins: [
+            [
+                'module:react-native-dotenv',
+                {
+                    moduleName: '@env',
+                    path: '.env.local',
+                    blacklist: null,
+                    whitelist: null,
+                    safe: false,
+                    allowUndefined: true,
+                },
+            ],
+        ],
+    };
 };
