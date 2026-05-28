3aa4575a8e5c553bc4b8e58d833049c3db00559d
Wilbert Chan <l22070034@valladolid.tecnm.mx>
2026-05-27 20:45:13 -0600

    Update sqlite.js

--- src/database/sqlite.js
+++ src/database/sqlite.js
@@ -1,46 +1,44 @@
-//importamos sql 
-import * as SQLite from "expo-sqlite";
-//importa plactofrm para detectar el sistema operativo
-import { Platform } from 'react-native';
+import { Platform } from "react-native";
 
+let db = null;
 
-//inicializamos el nombre de nuestra bd 
-const db = null;
+if (Platform.OS !== "web") {
 
+    const SQLite = require("expo-sqlite");
 
-//verificamos el sistema operativo para usar la ruta correcta de la base de datos
-if (Platform.OS !== 'web') {
-    db = SQLite.openDatabaseSync('smartlight.db');
+    db = SQLite.openDatabaseSync("smartlight.db");
+
 }
 
-
-//creamos la funcion para crear la tabla  e inicializamos 
 export const initDatabase = async () => {
 
-    //evitamos que se ejcute sin es en web 
-    if (Platform.OS === 'web') {
-        console.log("sqlite no disponible en web ");
+    if (Platform.OS === "web") {
+
+        console.log("SQLite no disponible en Web");
+
         return;
+
     }
 
     try {
-        await db.execAsync(
-            `CREATE TABLE IF NOT EXISTS dispositivos(
+
+        await db.execAsync(`
+            CREATE TABLE IF NOT EXISTS dispositivos (
                 id TEXT PRIMARY KEY NOT NULL,
                 nombre TEXT,
                 ubicacion TEXT,
                 estado INTEGER
-            ); `
-        );
+            );
+        `);
 
-        //debugiamos si hubo un error  
-        console.log("base de datos creamos correctamente");
+        console.log("Base de datos creada correctamente");
+
     } catch (error) {
-        console.error(
-            "error de conexion de base de datos", error
-        );
-    }
-}
 
-//exportamos la base de datos para usarla en otros archivos
+        console.error("Error al crear la base de datos:", error);
+
+    }
+
+};
+
 export default db;
